d30211b37befe473bc2c8212c12899977e7713b2
SoumyaSwaraj <43204618+SoumyaSwaraj@users.noreply.github.com>
2019-10-13 20:51:52 +0530

    Update admin.py

--- learning_users/basic_app/admin.py
+++ learning_users/basic_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from basic_app.models import UserProfileInfo
 
-# Register your models here.
+# Register your models here. Don't just delete them
 
 admin.site.register(UserProfileInfo)
